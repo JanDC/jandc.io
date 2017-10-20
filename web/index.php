89f86729e3bba2e0bf3936200dff467fe6f6d42d
Jan Decavele <jandecavele@gmail.com>
2017-10-20 17:03:38 +0200

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html >
+<html>
+<head>
+   <title>JanDC.io</title>
+</head>
+<body class="u-flex-set">
+<h1>My Stuff<h1>
+<a href="critical-css.jandc.io" >Critical css library</a>
+</body>
+</html>
