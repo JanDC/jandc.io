2aa01a81c80496b1bcd0253269125ff0b570f076
Jan Decavele <jandecavele@gmail.com>
2017-10-20 17:27:10 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,6 +5,6 @@
 </head>
 <body class="u-flex-set">
 <h1>My Stuff<h1>
-<a href="critical-css.jandc.io" >Critical css library</a>
+<a href="//critical-css.jandc.io" >Critical css library</a>
 </body>
 </html>
